b856183011f6d1c4b200ea166ecf6da0d2ee4d0b
Huzaifa0608 <121491679+Huzaifa0608@users.noreply.github.com>
2023-07-06 08:06:47 +0500

    Work Upadated

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.5/firebase-app.js";
-// import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.5/firebase-analytics.js";
-// import { getDatabase, push, set, ref, onValue, update, remove } from "https://www.gstatic.com/firebasejs/9.6.5/firebase-database.js";
-
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//     // ADD YOURS
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-
-// const Db = getDatabase()
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
